3dd45b12260db3c6407b247e85c2617665e3a90a
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-08-11 14:16:42 -0300

    sql files

---
+++ sqlfiles/satisfacao_tb_votos.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `satisfacao` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `satisfacao`;
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: satisfacao
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_votos`
+--
+
+DROP TABLE IF EXISTS `tb_votos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tb_votos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `dt_incl` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comentario` text,
+  `pontualidade` tinyint(4) NOT NULL,
+  `clareza` tinyint(4) NOT NULL,
+  `mtd_avaliacao` tinyint(4) NOT NULL,
+  `conhecimento` tinyint(4) NOT NULL,
+  `profid` bigint(20) unsigned NOT NULL,
+  `alunoid` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `proffk_idx` (`profid`),
+  KEY `alunofk_idx` (`alunoid`),
+  CONSTRAINT `alunofk` FOREIGN KEY (`alunoid`) REFERENCES `tb_aluno` (`id`),
+  CONSTRAINT `proffk` FOREIGN KEY (`profid`) REFERENCES `tb_professor` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-11 11:35:23
